bad46bb35376f7d19cb327856ce858987f114e5d
Shivam <shivamsharma0501@gmail.com>
2020-02-09 05:16:15 +0530

    $ fixed in node js

--- 1-node-farm/01-node-farm/index.js
+++ 1-node-farm/01-node-farm/index.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
 
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn);
+console.log(textIn); 
+
+const textOut = 'This is what we know about the avacado: '+textIn +'.\n Created on  ' +Date.now();
+fs.writeFileSync('./txt/output.txt',textOut);
+console.log('File Written!');
+
