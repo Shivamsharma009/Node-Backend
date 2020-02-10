c32f531d75df6cc9871420405b97e101c7c8e7fa
Shivam <shivamsharma0501@gmail.com>
2020-02-10 16:21:58 +0530

    Read Write File Asynchronouly

--- 1-node-farm/01-node-farm/index.js
+++ 1-node-farm/01-node-farm/index.js
@@ -1,9 +1,24 @@
 const fs = require('fs');
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn); 
+//const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+//console.log(textIn); 
 
-const textOut = 'This is what we know about the avacado: '+textIn +'.\n Created on  ' +Date.now();
-fs.writeFileSync('./txt/output.txt',textOut);
-console.log('File Written!');
+//const textOut = 'This is what we know about the avacado: '+textIn +'.\n Created on  ' +Date.now();
+//fs.writeFileSync('./txt/output.txt',textOut);
+//console.log('File Written!');
+
+//non-blocking synchronous way
+fs.readFile('./txt/start.txt','utf-8',()=> {
+    fs.readFile('./txt/read-this.txt','utf-8',(err,data2)=> {
+        console.log(data2);
+        fs.readFile('./txt/append.txt', 'utf-8' ,(err,data3) => {
+        console.log(data3);
+
+        fs.writeFile('./txt/final.txt', data2 +'\n'+ data3 ,'utf-8', err => {
+            console.log('Your File has been Written 😁😁')
+        })
+        });
+    });
+});
+console.log("Will read this!");
 
