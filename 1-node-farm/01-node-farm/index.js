6729ecf238baed5e785244e748111e10519dfddd
Shivam <shivamsharma0501@gmail.com>
2020-02-10 20:20:19 +0530

    Build Simple API

--- 1-node-farm/01-node-farm/index.js
+++ 1-node-farm/01-node-farm/index.js
@@ -30,6 +30,9 @@ const http = require('http');
 ////////////////////////////////////////
 //////SERVER
 
+const data = fs.readFileSync('./dev-data/data.json','utf-8')
+const dataobj = JSON.parse(data);
+
 const server = http.createServer((req,res) =>{
     const pathName = req.url;
 
@@ -37,7 +40,11 @@ const server = http.createServer((req,res) =>{
         res.end('This is the Overview');
     }else if(pathName === '/product'){
         res.end('This is the Product');
-    }else {
+    }else if(pathName === '/api'){  
+           res.writeHead(200,{'Content-type': 'application/json'});
+           res.end(data);
+       }  
+       else {
         res.writeHead(404 ,{
             'Content-type': 'text\html',
             'my-own-header': 'hello-world'
