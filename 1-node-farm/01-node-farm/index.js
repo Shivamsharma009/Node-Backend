0bcda94ec09c5ef5fb489be03bb92455b92efd3a
Shivam <shivamsharma0501@gmail.com>
2020-02-10 16:52:00 +0530

    Http server

--- 1-node-farm/01-node-farm/index.js
+++ 1-node-farm/01-node-farm/index.js
@@ -1,25 +1,40 @@
 const fs = require('fs');
+const http = require('http');
+
+////////////////////////////
+//FILES
 
 //blocking synchronous way
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn); 
+//const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+//console.log(textIn); 
 
-const textOut = 'This is what we know about the avacado: '+textIn +'.\n Created on  ' +Date.now();
-fs.writeFileSync('./txt/output.txt',textOut);
-console.log('File Written!');
+//const textOut = 'This is what we know about the avacado: '+textIn +'.\n Created on  ' +Date.now();
+//fs.writeFileSync('./txt/output.txt',textOut);
+//console.log('File Written!');
 
 //non-blocking synchronous way
-fs.readFile('./txt/start.txt','utf-8',()=> {
-    fs.readFile('./txt/read-this.txt','utf-8',(err,data2)=> {
-        console.log(data2);
-        fs.readFile('./txt/append.txt', 'utf-8' ,(err,data3) => {
-        console.log(data3);
+//fs.readFile('./txt/start.txt','utf-8',()=> {
+  //  fs.readFile('./txt/read-this.txt','utf-8',(err,data2)=> {
+    //    console.log(data2);
+      //  fs.readFile('./txt/append.txt', 'utf-8' ,(err,data3) => {
+        //console.log(data3);
 
-        fs.writeFile('./txt/final.txt', data2 +'\n'+ data3 ,'utf-8', err => {
-            console.log('Your File has been Written 😁😁')
-        })
-        });
-    });
+        //fs.writeFile('./txt/final.txt', data2 +'\n'+ data3 ,'utf-8', err => {
+          //  console.log('Your File has been Written 😁😁')
+       // })
+        //});
+    //});
+//});
+//console.log("Will read this!");
+
+////////////////////////////////////////
+//////SERVER
+
+const server = http.createServer((req,res) =>{
+    //console.log(req);
+    res.end('Hello from the Server!');
 });
-console.log("Will read this!");
 
+server.listen(8000,'localhost',() => {
+    console.log('Listening on the port 8000');
+});
