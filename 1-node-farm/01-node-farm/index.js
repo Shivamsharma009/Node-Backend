2e5b2c6c373f8be005ced5e12ef3ee1f746ddc68
Shivam <shivamsharma0501@gmail.com>
2020-02-10 16:23:33 +0530

    Remove //

--- 1-node-farm/01-node-farm/index.js
+++ 1-node-farm/01-node-farm/index.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 
-//const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-//console.log(textIn); 
+//blocking synchronous way
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn); 
 
-//const textOut = 'This is what we know about the avacado: '+textIn +'.\n Created on  ' +Date.now();
-//fs.writeFileSync('./txt/output.txt',textOut);
-//console.log('File Written!');
+const textOut = 'This is what we know about the avacado: '+textIn +'.\n Created on  ' +Date.now();
+fs.writeFileSync('./txt/output.txt',textOut);
+console.log('File Written!');
 
 //non-blocking synchronous way
 fs.readFile('./txt/start.txt','utf-8',()=> {
