eaafeb6cadf27f38b8e2f74e71531221a48a79f9
Shivam <shivamsharma0501@gmail.com>
2020-02-12 19:15:35 +0530

    Events

--- 1-node-farm/2-how-node-works/events.js
+++ 1-node-farm/2-how-node-works/events.js
@@ -1,5 +1,5 @@
 const EventEmitter = require("events");
-
+const http = require("http");
 
 class Sales extends EventEmitter {
     constructor() {
@@ -19,3 +19,22 @@ myEmitter.on('newSale', stock => {
     console.log("There are now  " + stock + "  item left in stock");
 })
 myEmitter.emit("newSale", 9);
+
+/////////////////////////////////////////////////////
+
+const server = http.createServer()
+server.on('request', (req, res) => {
+    console.log('Request Received!');
+    res.end('Request Received');
+})
+
+server.on("request", (req, res) => {
+    console.log("Another request 😀");
+})
+
+server.on("close", () => {
+    console.log('Server Closed');
+})
+server.listen(8000, "127.0.0.1", () => {
+    console.log("waiting for requests..............")
+})
